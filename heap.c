476e6469a5909a9397ad6e93d2f53c558b8c376e
edsontm <edsontm@gmail.com>
2025-03-24 19:59:12 -0400

    Update heap.c

--- heap.c
+++ heap.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<assert.h>
 
 /* DECLARACOES */
@@ -14,6 +15,28 @@ void constroi_heap(theap * heap, int max){
     heap->n = 0;
     heap->max = max;
 }
+void destroi_heap(theap * heap){
+    free(heap->v);
+    heap->v = NULL;
+    heap->n = 0;
+    heap->max = 0;
+}
+int pai(int i){
+    int ret = 0;
+    if (i>0)
+        ret = (i-1)/2;
+    return ret;
+}
+int filho_esq(int i){
+    return i*2 +1;
+}
+int filho_dir(int i){
+    return i*2 +2;
+}
+
+void desce(theap * heap,int i){
+}
+
 /* FUNCOES TESTE*/
 void test_constroi_heap(){
     /*inicializacao*/
@@ -27,11 +50,65 @@ void test_constroi_heap(){
     assert(heap.v != NULL);
     assert(heap.n == 0);
     assert(heap.max == 20);
+    free(heap.v);
+}
+
+void test_destroi_heap(){
+    /*inicializacao*/
+    theap heap;
+    int max = 20;
+    
+    /*chamada funcoes*/
+    constroi_heap(&heap,max);
+    destroi_heap(&heap);
+    
+    /*testes*/
+    assert(heap.v == NULL);
+    assert(heap.n == 0);
+    assert(heap.max == 0);
+}
+
+void test_pai(){
+    assert(pai(0)==0);
+    assert(pai(1)==0);
+    assert(pai(2)==0);
+    assert(pai(3)==1);
+    assert(pai(4)==1);
+}
+void test_filhos(){
+    assert(filho_esq(0)==1);
+    assert(filho_dir(0)==2);
+    assert(filho_esq(1)==3);
+    assert(filho_dir(1)==4);
+    assert(filho_esq(2)==5);
+    assert(filho_dir(2)==6);
+}
+void test_desce(){
+    /*inicializacao*/
+
+    theap heap;
+    int max = 20;
+    int vi[] = {3,20,10,15,17,9,7,4,5,8,2,11,12,13,14};
+    
+    /*chamada funcoes*/
+    constroi_heap(&heap,max);
+    heap.v = memcpy(heap.v,vi,sizeof(vi));
+    desce(&heap,0); 
+    /*testes*/
+    assert(heap.v[0]==20);
+    assert(heap.v[1]==17);
+    assert(heap.v[4]==8);
+    assert(heap.v[9]==3);
 }
 
 int main(void){
     test_constroi_heap();
+    test_destroi_heap();
+    test_pai();
+    test_filhos();
+    test_desce();
     printf("SUCESSO!\n");
     return EXIT_SUCCESS;
 }
+
 
